1913181a0ef6b115c7771d583e039246139d24c8
dennishvf <dennisk38@nycstudents.net>
2023-11-13 16:34:40 -0500

    m ,m ,

--- vite-project/js/Dom.js
+++ vite-project/js/Dom.js
@@ -1,3 +1,7 @@
 const DOMselectors = {
 FlexContainer: document.getElementById("flex-container"),
+ItemCard: document.querySelectorAll(".card"),
+ItemName: document.querySelectorAll(".card-title"),
+ItemImage: document.querySelectorAll(".card-img"),
+ItemDesc: ducmunet.querySelectorAll(".card-desc"),
 }
