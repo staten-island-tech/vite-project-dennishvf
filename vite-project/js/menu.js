8a147c17965af641ef95ed1afbcd7c97c6786f0b
dennishvf <dennisk38@nycstudents.net>
2023-11-14 09:24:19 -0500

    gfg

--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -1,3 +1,38 @@
-const menu = ["Shrimp Co","Eminems"];
-const name = "Denizee";
-export {menu, name}
+const menu = [
+{Name: "Shrimp Cocktail",
+Genre: "Appetizer",
+Picture: "https://theperfectportion.com/wp-content/uploads/2016/02/PerfectPortion-ShrimpCocktail.jpg",
+Price: "$7.00"
+},
+{Name: "Calamari",
+Genre: "Appetizer",
+Picture: "https://www.sizzlefish.com/cdn/shop/articles/Easy_Fried_Calamari_Alternate_Main.jpg?v=1678205798",
+Price: "$10"
+},
+{Name: "Mozzarela",
+Genre: "Appetizer",
+Picture: "https://www.zingermanscreamery.com/app/uploads/2018/04/zing-creamery-285.jpg",
+Price: "$4"
+},
+{Name: "Burger",
+Genre: "MC",
+Picture: "https://www.tastingtable.com/img/gallery/what-makes-restaurant-burgers-taste-different-from-homemade-burgers-upgrade/intro-1662064407.jpg",
+Price: "$13"
+},
+{Name: "Lasagna",
+Genre: "MC",
+Picture: "https://www.modernhoney.com/wp-content/uploads/2019/08/Classic-Lasagna-14-scaled.jpg",
+Price: "$18"
+},
+{Name:"Pizza",
+Genre: "MC",
+Picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Pizza-3007395.jpg/800px-Pizza-3007395.jpg",
+Price: "$14"
+},
+{Name: "Spaghetti",
+Genre: "MC",
+Picture: "https://urbanblisslife.com/wp-content/uploads/2023/01/Jolibee-Spaghetti-FEATURE.jpg",
+Price: "$16"
+},
+]
+export {menu}
