2c37f566f590de527271ba9f017ea81d01222604
dennishvf <dennisk38@nycstudents.net>
2023-11-16 09:24:30 -0500

    jjtgjgt

--- vite-project/js/Dom.js
+++ vite-project/js/Dom.js
@@ -1,11 +1,12 @@
 const DOMselectors = {
 Form: document.getElementById("ButtonForm"),
-AppButton: document.getElementById("Appb"),
+AppeButton: document.getElementById("Appb"),
 MCButton: document.getElementById("MCb"),
 DessertButton: document.getElementById("Db"),
-FlexContainer: document.getElementById("flex-container"),
+FlexContainer: document.querySelector(".flex-container"),
 ItemCard: document.querySelectorAll(".card"),
 ItemName: document.querySelectorAll(".card-title"),
 ItemImage: document.querySelectorAll(".card-img"),
-ItemDesc: ducmunet.querySelectorAll(".card-desc"),
+ItemDesc: docmunet.querySelectorAll(".card-price"),
 }
+export {DOMselectors}
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,6 +1,30 @@
 import "../css/style.css";
 import {menu} from "./menu";
+import { DOMselectors } from "./Dom";
+//information for each page
+const Appe = menu.filter((menu) => menu.Genre === "Appetizer");
+const main = menu.filter((menu) => menu.Genre === "MC");
+const des = menu.filter((menu) => menu.Genre === "Dessert");
 
+//mking the cards
+function Mealinfo() {
+    const make= {
+        name:DOMselectors.AniNameform.value,
+         img:DOMselectors.AniImgform.value,
+         price:DOMselectors.AniDescform.value,}
+    return make
+    }
+function MakeCards(Meal){
+    document.querySelector(".flex-container").insertAdjacentHTML("afterbegin", `<div class="card">
+<h1 class="card-title">${Meal.name}</h1>
+<img src="${Meal.img}" alt="" class="card-img">
+<h2 class="animal-desc">${Meal.price}</h2>
+</div>`)
+}
 
-const App = data.filter((menu) => menu.Genre === "Appetizer");
-console.log(App);
+//Button work
+DOMselectors.ButtonForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const Concard = Mealinfo()
+    MakeCards(Concard)
+})
